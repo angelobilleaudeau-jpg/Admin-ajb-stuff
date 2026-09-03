ef87429cb70bcc21371a58cb8035b9200350d2cc
Aj <angelobilleaudeau@gmail.com>
2026-09-02 21:26:12 -0500

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,2 +1,11 @@
-cd C:\goober\prjx-frontend-patches\Project-X-V2\Roblox\Roblox.Website
-dotnet run --configuration Release
+@echo off
+echo Running npm run build...
+call npm run build
+
+if %errorlevel% neq 0 (
+    echo Build failed with error code %errorlevel%.
+    exit /b %errorlevel%
+)
+
+echo Build completed successfully. Starting the application...
+call npm run start
